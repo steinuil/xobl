d642224e910688ca226bdd4386f0e713ea5e55ba
steinuil <steenuil.owl@gmail.com>
2023-06-18 14:44:35 +0200

    remove old shell.nix

--- shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell rec {
-  buildInputs = with pkgs; [ clang m4 binutils pkgconfig gmp autoconf libseccomp inotify-tools glibc ];
-
-  shellHook = ''
-    eval $(opam env)
-  '';
-}
