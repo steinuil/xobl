1eca168a9cde81d4a5d811c3c9009c342418970b
steinuil <steenuil.owl@gmail.com>
2024-07-22 18:31:15 +0200

    use optint for 32-bit ints

--- src/protocol/types.ml
+++ src/protocol/types.ml
@@ -8,10 +8,18 @@ type byte = char [@@deriving sexp]
 type nonrec bool = bool [@@deriving sexp]
 type i8 = int [@@deriving sexp]
 type i16 = int [@@deriving sexp]
-type i32 = int [@@deriving sexp]
+type i32 = Optint.t
+
+let sexp_of_i32 n = Optint.to_int32 n |> sexp_of_int32
+let i32_of_sexp n = int32_of_sexp n |> Optint.of_int32
+
 type u8 = int [@@deriving sexp]
 type u16 = int [@@deriving sexp]
-type u32 = int [@@deriving sexp]
+type u32 = Optint.t
+
+let sexp_of_u32 n = Optint.to_unsigned_int32 n |> sexp_of_int32
+let u32_of_sexp n = int32_of_sexp n |> Optint.of_unsigned_int32
+
 type u64 = int64 [@@deriving sexp]
 type nonrec float = float [@@deriving sexp]
 type double = float [@@deriving sexp]
